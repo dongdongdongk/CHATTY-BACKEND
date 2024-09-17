618ec5348ab506bbe8eb1071466d327c48628d68
dhk939393 <dhk9309@gmail.com>
2024-09-18 00:36:10 +0900

    서버세팅

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -1,5 +1,21 @@
-import { Application, json, urlencoded, Response, Request, NextFunction } from 'express'
-import http from 'http';
+import {
+    Application,
+    json,
+    urlencoded,
+    Response,
+    Request,
+    NextFunction,
+    application,
+} from "express";
+import http from "http";
+import cors from "cors";
+import helmet from "helmet";
+import hpp from "hpp";
+import cookierSession from "cookie-session";
+import HTTP_STATUS from "http-status-codes";
+import compression from 'compression'
+import "express-async-errors";
+
 export class ChattyServer {
     private app: Application;
 
@@ -15,18 +31,40 @@ export class ChattyServer {
         this.startServer(this.app);
     }
 
-    private securityMiddleware(app: Application): void {}
-    
-    private standardMiddleware(app: Application): void {}
-    
-    private routeMiddleware(app: Application): void {}
-    
-    private globalErrorHandler(app: Application): void {}
-    
-    private startServer(app: Application): void {}
-    
-    private createSocketIO(httpServer: http.Server): void {}
-    
-    private startHttpServer(httpServer: http.Server): void {}
+    private securityMiddleware(app: Application): void {
+        app.use(
+            cookierSession({
+                name: "session",
+                keys: ["text1", "test2"],
+                maxAge: 24 * 7 * 360000,
+                secure: false,
+            })
+        );
+        app.use(hpp());
+        app.use(helmet());
+        app.use(
+            cors({
+                origin: '*',
+                credentials: true,
+                optionsSuccessStatus: 200,
+                methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+            })
+        );
+    }
 
+    private standardMiddleware(app: Application): void {
+        app.use(compression());
+        app.use(json({ limit: '50mb'}));
+        app.use(urlencoded({ extended: true, limit: '50mb'}));
+    }
+
+    private routeMiddleware(app: Application): void { }
+
+    private globalErrorHandler(app: Application): void { }
+
+    private startServer(app: Application): void { }
+
+    private createSocketIO(httpServer: http.Server): void { }
+
+    private startHttpServer(httpServer: http.Server): void { }
 }
