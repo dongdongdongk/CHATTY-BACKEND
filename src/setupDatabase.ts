f12da7fdbd0992b2bcfc77962af9fa09e394300a
dhk939393 <dhk9309@gmail.com>
2024-10-02 00:19:06 +0900

    테스트

--- src/setupDatabase.ts
+++ src/setupDatabase.ts
@@ -1,22 +1,23 @@
-    import mongoose from "mongoose";
-    import { config } from "./config";
+import mongoose from 'mongoose';
+import { config } from './config';
 
-    import Logger from "bunyan";
+import Logger from 'bunyan';
 
-    const log: Logger = config.createLogger('setupDatabase')
+const log: Logger = config.createLogger('setupDatabase');
 
-    export default () => {
-        const connect = () => {
-            mongoose.connect( `${config.DATABASE_URL}`)
-                .then(() => {
-                    log.info('Successfully connected to database.')
-                })
-                .catch((error) =>{
-                    log.error('Error connecting to database', error);
-                    return process.exit(1);
-                })
-        };
-        connect();  
+export default () => {
+  const connect = () => {
+    mongoose
+      .connect(`${config.DATABASE_URL}`)
+      .then(() => {
+        log.info('Successfully connected to database.');
+      })
+      .catch((error) => {
+        log.error('Error connecting to database', error);
+        return process.exit(1);
+      });
+  };
+  connect();
 
-        mongoose.connection.on('disconnected', connect);
-    }
+  mongoose.connection.on('disconnected', connect);
+};
