786401261f9a04f0dde3febe63c4cf2fe06d0729
dongdongdongk <dhk9309@gmail.com>
2024-10-08 18:17:42 +0900

    joi

---
+++ src/shared/globals/decorators/joi-validation.decorator.ts
@@ -0,0 +1,22 @@
+import { JoiRequestValidationError } from '@global/helpers/error-handler';
+import { Request } from 'express';
+import { ObjectSchema } from 'joi';
+
+type IjoiDecorator = ( target: any, key: string, descriptor: PropertyDescriptor ) => void;
+
+export function joiValidation(schema: ObjectSchema): IjoiDecorator {
+  return (_target: any, _key: string, descriptor: PropertyDescriptor ) => {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = async function (...args: any[]) {
+      const req: Request = args[0];
+      // validateAsync 또는 validate 둘다 사용가능 
+      const { error } = await Promise.resolve(schema.validate(req.body));
+      if ( error?.details) {
+        throw new JoiRequestValidationError(error.details[0].message);
+      }
+      return originalMethod.apply(this, args);
+    };
+    return descriptor;
+  };
+}
