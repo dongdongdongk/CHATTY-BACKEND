929fdbac9a8cccf67cbb3e55fff89dbfcb51b7e2
dongdongdongk <dhk9309@gmail.com>
2024-10-16 13:27:39 +0900

    레디스 설정중

---
+++ src/shared/services/redis/redis.connection.ts
@@ -0,0 +1,23 @@
+import Logger from 'bunyan';
+import { config } from '@root/config';
+import { BaseChache } from '@service/redis/base.caches';
+
+const log: Logger = config.createLogger('redisConnection');
+
+class RedisConnection extends BaseChache {
+  constructor() {
+    super('redisConnection');
+  }
+
+  async connect(): Promise<void> {
+    try {
+      await this.client.connect();
+      const res = await this.client.ping();
+      console.log(res,'redis');
+    } catch (error) {
+      log.error(error);
+    }
+  }
+}
+
+export const redisConnection: RedisConnection = new RedisConnection();
