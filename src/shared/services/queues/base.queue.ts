2104e1397f765a9b06f51f41cbe2caf00fd0d26b
dongdongdongk <dhk9309@gmail.com>
2024-10-22 21:03:24 +0900

    큐세팅중

---
+++ src/shared/services/queues/base.queue.ts
@@ -0,0 +1,22 @@
+import Queue, {Job } from 'bull';
+import Logger from 'bunyan';
+import { createBullBoard } from '@bull-board/api';
+import { BullAdapter } from '@bull-board/api/bullAdapter';
+import { ExpressAdapter } from '@bull-board/express';
+import { config } from '@root/config';
+
+
+let bullAdapter: BullAdapter[] = [];
+
+export let serverAdapter: ExpressAdapter;
+
+export abstract class BaseQueue {
+  queue: Queue.Queue;
+  log: Logger;
+
+  constructor(queueName: string) {
+    this.queue = new Queue(queueName, `${config.REDIS_HOST}`);
+    bullAdapter.push(new BullAdapter)
+  }
+  
+}
