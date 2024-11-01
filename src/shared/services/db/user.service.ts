b12265c61847158b269513bf798a7053aa75748d
dongdongdongk <dhk9309@gmail.com>
2024-11-01 12:28:29 +0900

    몽고 DB 데이터 가져오기

--- src/shared/services/db/user.service.ts
+++ src/shared/services/db/user.service.ts
@@ -1,10 +1,19 @@
 import { IUserDocument } from '@user/interfaces/user.interface';
 import { UserModel } from '@user/models/user.schema';
+import mongoose from 'mongoose';
 
 class UserService {
 
   public async addUserData(data: IUserDocument): Promise<void> {
     await UserModel.create(data);
+  }
+
+  public async getUserById(userId: string): Promise<IUserDocument> {
+    const users: IUserDocument[] = await UserModel.aggregate([
+      { $match: { _id: new mongoose.Types.ObjectId(userId)} },
+      { $lookup: { from: 'Auth', localField: 'authId', foreignField: '_id', as: 'authId'} },
+      { $unwind: '$authId'}
+    ]);
   }
 }
 
