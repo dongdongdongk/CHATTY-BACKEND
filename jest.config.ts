cdc413010549f697f6dbe9e7b964dbb1f536cb08
dongdongdongk <dhk9309@gmail.com>
2024-11-27 13:40:47 +0900

    post 스키마,모델,인터페이스 작성중

--- jest.config.ts
+++ jest.config.ts
@@ -27,6 +27,7 @@ const config: Config.InitialOptions = {
     '@global/(.*)': '<rootDir>/src/shared/globals/$1',
     '@service/(.*)': '<rootDir>/src/shared/services/$1',
     '@socket/(.*)': '<rootDir>/src/shared/sockets/$1',
+    '@post/(.*)': '<rootDir>/src/shared/post/$1',
     '@worker/(.*)': '<rootDir>/src/shared/workers/$1',
     '@root/(.*)': '<rootDir>/src/$1',
   },
