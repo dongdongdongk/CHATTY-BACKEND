4300cb40771fdcd150a06852e9b0e59c83e71f2e
dongdongdongk <dhk9309@gmail.com>
2024-12-16 10:07:16 +0900

    포스트 이미지 추가 작성

--- src/features/post/controllers/create-post.ts
+++ src/features/post/controllers/create-post.ts
@@ -1,9 +1,12 @@
 import { joiValidation } from '@global/decorators/joi-validation.decorator';
+import { upload } from '@global/helpers/cloudinary-upload';
+import { BadRequestError } from '@global/helpers/error-handler';
 import { IPostDocument } from '@post/interfaces/post.interface';
-import { postSchema } from '@post/schemes/post.schemes';
+import { postSchema, postWithImageSchema } from '@post/schemes/post.schemes';
 import { postQueue } from '@service/queues/post.queue';
 import { PostCashe } from '@service/redis/post.cache';
 import { soketIOPostObject } from '@socket/post';
+import { UploadApiResponse } from 'cloudinary';
 import { Request, Response } from 'express';
 import HTTP_STATUS from 'http-status-codes';
 import { ObjectId } from 'mongodb';
@@ -48,4 +51,50 @@ export class Create {
 
     res.status(HTTP_STATUS.CREATED).json({message: 'Post created successfully'});
   }
+
+
+  @joiValidation(postWithImageSchema)
+  public async postWithImage(req: Request, res: Response): Promise<void> {
+    const { post, bgColor, privacy, gifUrl, profilePicture, feelings, image } = req.body;
+
+    const result: UploadApiResponse = (await upload(image)) as UploadApiResponse;
+    if (!result?.public_id) {
+      throw new BadRequestError(result.message);
+    }
+    
+    const postObjectId: ObjectId = new ObjectId();
+    const createdPost: IPostDocument = {
+      _id: postObjectId,
+      userId: req.currentUser!.userId,
+      username: req.currentUser!.username,
+      email: req.currentUser!.email,
+      avatarColor: req.currentUser!.avatarColor,
+      profilePicture,
+      post,
+      bgColor,
+      feelings,
+      privacy,
+      gifUrl,
+      commentsCount: 0,
+      imgVersion: result.version.toString(),
+      imgId: result.public_id,
+      createdAt: new Date(),
+      reactions: { like: 0, love: 0, happy: 0, sad: 0, wow: 0, angry: 0 }
+    } as IPostDocument;
+    
+    soketIOPostObject.emit('add post', createdPost);
+
+    await postCache.savePostToCache({
+      key: postObjectId,
+      currentUserId: `${req.currentUser!.userId}`,
+      uId: `${req.currentUser!.uId}`,
+      createdPost
+    });
+    postQueue.addPostJob('addPostToDB', { key: req.currentUser!.userId, value: createdPost});
+    // call image queue to add image to mongodb database
+
+    res.status(HTTP_STATUS.CREATED).json({message: 'Post created with image successfully'});
+  }
+
+  
 }
