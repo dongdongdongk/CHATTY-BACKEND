089d2b022e22dadecc51c94a90bbfd721ba7e1df
dongdongdongk <dhk9309@gmail.com>
2024-12-19 10:03:17 +0900

    포스트 캐쉬 여러 메서드 추가

--- src/shared/services/redis/post.cache.ts
+++ src/shared/services/redis/post.cache.ts
@@ -4,8 +4,12 @@ import { config } from '@root/config';
 import { ServerError } from '@global/helpers/error-handler';
 import { IPostDocument, IReactions, ISavePostToCache } from '@post/interfaces/post.interface';
 import { Helpers } from '@global/helpers/heplers';
+import { RedisCommandRawReply } from '@redis/client/dist/lib/commands';
 
 const log: Logger = config.createLogger('postCache');
+
+
+export type PostCacheMultiType = string | number | Buffer | RedisCommandRawReply[] | IPostDocument | IPostDocument[]
 
 export class PostCache extends BaseCache {
   constructor() {
@@ -102,6 +106,7 @@ export class PostCache extends BaseCache {
     }
   }
 
+  
   public async getPostFromCashe(key: string, start: number, end: number): Promise<IPostDocument[]> {
     try {
       if (!this.client.isOpen) {
@@ -113,8 +118,8 @@ export class PostCache extends BaseCache {
       for( const value of reply) {
         multi.HGETALL(`post${value}`);
       }
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const replies:any = await multi.exec();
+
+      const replies: PostCacheMultiType = await multi.exec() as PostCacheMultiType;
       const postReplies: IPostDocument[] = [];
       for(const post of replies as IPostDocument[]) {
         post.commentsCount = Helpers.parseJson(`${post.commentsCount}`) as number;
@@ -123,6 +128,95 @@ export class PostCache extends BaseCache {
         postReplies.push(post);
       }
       return postReplies;
+
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error. Try again.');
+    }
+  }
+
+  public async getTotalPostsInCashe(): Promise<number> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+
+      const count: number = await  this.client.ZCARD('post');
+      return count;
+
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error. Try again.');
+    }
+  }
+
+
+  public async getPostsWithImagesFromCashe(key: string, start: number, end: number): Promise<IPostDocument[]> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+
+      const reply: string[] = await this.client.ZRANGE(key, start, end, {REV : true}); 
+      const multi: ReturnType<typeof this.client.multi> = this.client.multi();
+      for( const value of reply) {
+        multi.HGETALL(`post${value}`);
+      }
+
+      const replies: PostCacheMultiType = await multi.exec() as PostCacheMultiType;
+      const postWithImage: IPostDocument[] = [];
+      for(const post of replies as IPostDocument[]) {
+        if ((post.imgId && post.imgVersion) || post.gifUrl) {
+          post.commentsCount = Helpers.parseJson(`${post.commentsCount}`) as number;
+          post.reactions = Helpers.parseJson(`${post.reactions}`) as IReactions;
+          post.createdAt = Helpers.parseJson(`${post.createdAt}`) as Date;
+          postWithImage.push(post);
+        }
+      }
+      return postWithImage;
+
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error. Try again.');
+    }
+  }
+
+  public async getUserPostFromCashe(key: string, uId: number): Promise<IPostDocument[]> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+
+      const reply: string[] = await this.client.ZRANGE(key, uId, uId, {REV : true, BY: 'SCORE'}); 
+      const multi: ReturnType<typeof this.client.multi> = this.client.multi();
+      for( const value of reply) {
+        multi.HGETALL(`post${value}`);
+      }
+
+      const replies: PostCacheMultiType = await multi.exec() as PostCacheMultiType;
+      const postReplies: IPostDocument[] = [];
+      for(const post of replies as IPostDocument[]) {
+        post.commentsCount = Helpers.parseJson(`${post.commentsCount}`) as number;
+        post.reactions = Helpers.parseJson(`${post.reactions}`) as IReactions;
+        post.createdAt = Helpers.parseJson(`${post.createdAt}`) as Date;
+        postReplies.push(post);
+      }
+      return postReplies;
+
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error. Try again.');
+    }
+  }
+
+  public async getTotalUserPostsInCashe(uId: number): Promise<number> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+
+      const count: number = await  this.client.ZCOUNT('post', uId, uId);
+      return count;
 
     } catch (error) {
       log.error(error);
