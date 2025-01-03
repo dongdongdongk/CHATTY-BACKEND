ce79877c13234721fffb08d1fd716b977c9a86b6
dongdongdongk <dhk9309@gmail.com>
2025-01-03 13:45:04 +0900

    post 삭제 추가

--- src/shared/services/queues/post.queue.ts
+++ src/shared/services/queues/post.queue.ts
@@ -6,11 +6,13 @@ class PostQueue extends BaseQueue {
   constructor() {
     super('posts');
      this.processJob('addPostToDB', 5, postWorker.savePostToDB);
+     this.processJob('deletePostFromBD',5, postWorker.deletePostFromDB);
   }
 
   public addPostJob(name: string, data: IPostJobData): void {
     this.addJob(name, data);
   }
+
 }
 
 export const postQueue: PostQueue = new PostQueue();
