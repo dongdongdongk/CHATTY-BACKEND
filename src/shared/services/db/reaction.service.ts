9e9faced620b4c9bd3da9000c0653dbf6dab41e9
dongdongdongk <dhk9309@gmail.com>
2025-01-17 17:23:08 +0900

    리액션 서비스 추가

---
+++ src/shared/services/db/reaction.service.ts
@@ -0,0 +1,31 @@
+import { IPostDocument } from '@post/interfaces/post.interface';
+import { PostModel } from '@post/models/post.schema';
+import { IReactionDocument, IReactionJob } from '@reaction/interfaces/reaction.interface';
+import { ReactionModel } from '@reaction/models/reaction.schema';
+import { UserCache } from '@service/redis/user.cache';
+import { IUserDocument } from '@user/interfaces/user.interface';
+
+const userCache: UserCache = new UserCache();
+
+class ReactionService {
+  public async addReactionDataToDB(reactionData: IReactionJob): Promise<void> {
+    const { postId, userTo, userForm, username, type, previousReaction, reactionObject } = reactionData;
+    const updatedReaction: [IUserDocument, IReactionDocument, IPostDocument] = await Promise.all([
+      userCache.getUserFromCache(`${userTo}`),
+      ReactionModel.findOneAndReplace({ postId, type: previousReaction, username}, reactionObject, { upsert: true}),
+      PostModel.findOneAndUpdate(
+        { _id: postId },
+        {
+          $inc: {
+            [`reactions.${previousReaction}`]: -1,
+            [`reactions.${type}`]: 1
+          }
+        },
+        { new: true }
+      )
+    ]) as [IUserDocument, IReactionDocument, IPostDocument];
+    // send reaciton notification
+  }
+}
+
+export const reactionService: ReactionService = new ReactionService();
